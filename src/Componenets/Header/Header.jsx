1fbc2b9404195754447a480f2d92da517a6cf05b
Sasa Milicic <sasamilicicbdp@gmail.com>
2022-04-19 08:55:48 +0200

    Header UI

---
+++ src/Componenets/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StHeader } from './StyleHeader';
+
+function Header() {
+  return (
+    <StHeader>
+      <h1>Top Stories - Hacker News</h1>
+    </StHeader>
+  );
+}
+
+export default Header;
---
+++ src/Componenets/Header/StyleHeader.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StHeader = styled.header`
+  font-size: calc(10px + 2vmin);
+  color: black;
+`;
