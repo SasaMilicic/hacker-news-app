7f56342c74e58eb77c6edd433d21d55e8ff85086
Sasa Milicic <sasamilicicbdp@gmail.com>
2022-04-22 13:00:46 +0200

    update getting data variables

--- src/components/Main/story-page/MainStoryPage.jsx
+++ src/components/Main/story-page/MainStoryPage.jsx
@@ -2,26 +2,27 @@ import { StMain, StHeadline } from './style-story-page.js';
 import { useSelector } from 'react-redux';
 
 function MainStoryPage() {
-  const storyData = useSelector(state => state.topStory.value);
-  const renderAuthor = useSelector(() => storyData.by);
-  const renderUrl = useSelector(() => storyData.url);
-  const renderTitle = useSelector(() => storyData.title);
-  const renderNumComm = useSelector(() => storyData.kids.length);
-  const renderScore = useSelector(() => storyData.score);
+  const {
+    by: author,
+    url: toUrl,
+    title,
+    kids: comments,
+    score,
+  } = useSelector(state => state.topStory.value);
 
   return (
     <StMain>
       <StHeadline>
-        <h4> author: {renderAuthor} </h4>
+        <h4> author: {author} </h4>
         <h2>
-          <a target="_blank" rel="noreferrer" href={renderUrl}>
-            {renderTitle}
+          <a target="_blank" rel="noreferrer" href={toUrl}>
+            {title}
           </a>
         </h2>
       </StHeadline>
       <div>
         <h3>
-          {renderNumComm} comments | score: {renderScore}
+          {comments.length} comments | score: {score}
         </h3>
       </div>
     </StMain>
