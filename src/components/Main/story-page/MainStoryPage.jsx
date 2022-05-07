472a60893fdf25f7e59cdd96eead995224fe3366
Sasa Milicic <sasamilicicbdp@gmail.com>
2022-05-07 23:44:13 +0200

    render all stories to UI

--- src/components/Main/story-page/MainStoryPage.jsx
+++ src/components/Main/story-page/MainStoryPage.jsx
@@ -2,18 +2,18 @@ import * as S from './style-story-page';
 import { useSelector } from 'react-redux';
 
 function Main() {
-  const stateTopStories = useSelector(state => state.topStories);
+  const stateTopStories = useSelector(state => state.stories);
+
   const arrTopStories = stateTopStories.items;
-  let numArtical = stateTopStories.pageIndex + 1;
 
   const listStories = arrTopStories.map(
-    ({ by: author, id, url, title, score, kids: comments }) => (
+    ({ by: author, id, url, title, score, kids: comments }, i) => (
       <li key={id}>
         <S.Headline>
           <h4>author: {author} </h4>
           <h2>
             <a target="_blank" rel="noreferrer" href={url}>
-              {numArtical++}) <span>{title}</span>
+              {i + 1}) <span>{title}</span>
             </a>
           </h2>
         </S.Headline>
@@ -27,11 +27,13 @@ function Main() {
     )
   );
 
-  return (
-    <S.Main>
-      <ul>{listStories}</ul>
-    </S.Main>
-  );
+  const renderStories = () => {
+    if (stateTopStories.loading === true) return <h1>L O A D I N G . . .</h1>;
+
+    return <ul>{listStories}</ul>;
+  };
+
+  return <S.Main>{renderStories()}</S.Main>;
 }
 
 export default Main;
