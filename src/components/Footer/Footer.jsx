18e71562f545f2e6466c190358cb6c6f0bf6ebaf
unknown <sasamilicicbdp@gmail.com>
2022-05-10 21:36:23 +0200

    add switch page property

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,34 @@
-import React, { useEffect } from 'react';
 import * as S from './style-footer';
+import { useState, useEffect } from 'react';
 import { ReactComponent as ButtonPrevPage } from './svg/arrow-left-square-fill.svg';
 import { ReactComponent as ButtonNextPage } from './svg/arrow-right-square-fill.svg';
 import { actNextPage, actPrevPage } from '../../state/reducers-actions';
 import { useDispatch, useSelector } from 'react-redux';
 
 function Footer() {
-  const { pageNumber } = useSelector((state) => state.stories);
   const dispatch = useDispatch();
+  const { firstSliceIndex } = useSelector((state) => state.stories);
+  const [pageNumber, setPageNumber] = useState('');
+  const [checkIndex, setCheckIndex] = useState('');
+
+  const switchPage = () => {
+    if (firstSliceIndex > checkIndex) {
+      setPageNumber(pageNumber + 1);
+      setCheckIndex(firstSliceIndex);
+      return;
+    }
+    if (firstSliceIndex < checkIndex) {
+      setPageNumber(pageNumber - 1);
+      setCheckIndex(firstSliceIndex);
+      return;
+    }
+    setPageNumber(1);
+    setCheckIndex(firstSliceIndex);
+  };
+
+  useEffect(() => {
+    switchPage();
+  }, [firstSliceIndex]);
 
   return (
     <S.ContButttons>
